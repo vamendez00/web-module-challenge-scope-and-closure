90955d4cd2ef54edc7b4793c29ecf9944a88a45f
vamendez00 <veromendez@gmail.com>
2021-02-23 11:27:33 -0600

    1.3.1

--- index.js
+++ index.js
@@ -62,8 +62,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  return Math.floor(Math.random()*Math.floor(3));
 }
 
 
@@ -81,8 +81,20 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningCB, inningsPlayed){
+  let currentScore = {
+    Away: inningCB(),
+    Home: inningCB(),
+  };
+  
+  let homeScore = 0;
+  let awayScore = 0;
+  
+  for(let i = 0; i < inningsPlayed; i++){
+    homeScore = homeScore + currentScore.Home;
+    awayScore = awayScore + currentScore.Away;
+  }
+  return currentScore;
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -90,8 +102,11 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  return {
+    Home: inningCB(),
+    Away: inningCB()
+}
 }
 
 
